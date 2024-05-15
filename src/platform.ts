a6561150a43e465383dc9105ffaa77d936c3765e
Luligu <132135057+Luligu@users.noreply.github.com>
2024-05-15 09:48:34 +0200

    CHANGELOG.md

--- src/platform.ts
+++ src/platform.ts
@@ -60,6 +60,6 @@ export class EveDoorPlatform extends MatterbridgeAccessoryPlatform {
     this.log.info('onShutdown called with reason:', reason ?? 'none');
     await this.history?.close();
     clearInterval(this.interval);
-    await this.unregisterAllDevices();
+    if (this.config.unregisterOnShutdown === true) await this.unregisterAllDevices();
   }
 }
